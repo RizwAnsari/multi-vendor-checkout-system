975d874ce9311a94b9b72915da49bb2cfcd785c7
cml_rizwan_ansari <rizwan.a@callmedilife.com>
2026-03-24 01:15:14 +0530

    create order_items table migration

---
+++ database/migrations/2026_03_23_193406_create_order_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->string('product_name');
+            $table->decimal('unit_price', 10, 2);
+            $table->unsignedInteger('quantity');
+            $table->decimal('line_total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
