abd0b252073f428f557740e09a104c8283b23797
cml_rizwan_ansari <rizwan.a@callmedilife.com>
2026-03-24 01:22:09 +0530

    create payments table migration

---
+++ database/migrations/2026_03_23_194928_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->unique()->constrained();
+            $table->decimal('amount', 10, 2);
+            $table->enum('status', ['pending', 'paid', 'failed', 'refunded'])->default('pending')->index();
+            $table->string('transaction_ref')->nullable()->unique();
+            $table->timestamp('paid_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
