f62b390ea149a38174438166454b3054975a6d47
cml_rizwan_ansari <rizwan.a@callmedilife.com>
2026-03-23 21:52:01 +0530

    create vendors table

---
+++ database/migrations/2026_03_23_161619_create_vendors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            // default false, because vendor will be active after verification and approval
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
